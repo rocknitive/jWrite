4af62830e40e1dd508072930b0534a5ba0f30449
Jan Christoph Bernack <jc.bernack@gmail.com>
2019-06-26 18:43:16 +0200

    remove redeclaration of (u)int32_t

--- jWrite.c
+++ jWrite.c
@@ -15,10 +15,7 @@
 
 #include "jWrite.h"
 
-//#include <stdint.h>			// definintion of uint32_t, int32_t
-typedef unsigned int uint32_t;
-typedef int int32_t;
-
+#include <stdint.h>			// definintion of uint32_t, int32_t
 
 // the jWrite functions take the above jWriteControl structure pointer
 // to maintain state while writing a JSON string.
